ea90bed6914530864aa5052d92e1f730ebd288ca
colJY <ljy162518@gmail.com>
2023-01-02 03:37:15 +0900

    fromJson

--- lib/servoces/api_service.dart
+++ lib/servoces/api_service.dart
@@ -1,18 +1,26 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:webtoon/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     // async(비동기) 프로그래밍 -> 어떤 일이 일어날 때까지 기다림
     final url = Uri.parse('$baseUrl/$today');
     final response = await http
         .get(url); // 기다리게 하고 싶으면 await사용, await를 사용하기 위해서는 async 함수 내에서만 사용 가능
 
     if (response.statusCode == 200) {
-      print(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+
+      return webtoonInstances;
     }
     throw Error();
   }
